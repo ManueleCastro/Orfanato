6ab4e60d45748bc4879f5346759cbbe4fa9dd5ef
ManueleCastro <manucastrocontato@hotmail.com>
2021-01-31 17:28:56 -0300

    front end done

---
+++ public/scripts/page-orfanatos.js
@@ -0,0 +1,47 @@
+// create map
+
+const map = L.map("mapid").setView([-8.0595507, -34.8858846], 15);
+
+// create and add tilelayer
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "/images/map-marker.svg",
+  iconsize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+function addMarker({id, name, lat, lng}) { 
+
+  // create popup overlay
+  const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(`${name} <a href="/orfanato?id=${id}"> <img src="/images/arrow-white.svg"> </a>`);
+
+  // create and add marker
+
+  L.marker([lat,lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)
+}
+
+const orfanatosSpan = document.querySelectorAll('.orfanatos span')
+orfanatosSpan.forEach(span => {
+    const orfanato = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orfanato)
+
+})
+
+
---
+++ src/database/fakedata.js
@@ -0,0 +1,49 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-8.0595507",
+        lng: "-34.8858846",
+        name: "Lar das meninas depressivas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1532636653654-e243fbe850b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1502802729233-6bc93ca7ad71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1555009393-f20bdb245c4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1536640712-4d4c36ff0e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1519340241574-2cec6aef0c01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            ],
+        instructions: "Venha se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 8h até 18h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-8.0595507",
+        lng: "-34.8958846",
+        name: "Lar das crianças",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1532636653654-e243fbe850b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1502802729233-6bc93ca7ad71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1555009393-f20bdb245c4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1536640712-4d4c36ff0e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1519340241574-2cec6aef0c01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+            "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            ],
+        instructions: "Venha se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 8h até 18h",
+        open_on_weekends: "0"
+    }
+]
